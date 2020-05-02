dc0fbcc416908cf77c449f13068d1793b7bb3c09
DavidKozdra <45106564+DavidKozdra@users.noreply.github.com>
2020-05-02 17:53:30 -0400

    Set up the notification for new API level and saved minposition after close

--- app/src/main/java/com/example/mainactivity/MainActivity.java
+++ app/src/main/java/com/example/mainactivity/MainActivity.java
@@ -1,30 +1,34 @@
 package com.example.mainactivity;
 
-
+import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
-import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.NotificationCompat;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 import 	android.os.PowerManager;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
  //   PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
-    float MinimumPosition = -10f; //lowest y value allowed set to -10 on default stopping the notifications from starting automatically
-    private SensorManager sensorManager; // android class to handle all sensor types and data
+    float MinimumPosition = 0f; //lowest y value allowed set to -10 on default stopping the notifications from starting automatically
+    final String KEY ="MIN_KEY";
     Sensor accelerometer; // sensor of position and rate of movement
     TextView Y_Value; // android UI text item set to the current y value
     TextView cPosition;
@@ -40,16 +44,33 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         SelectMinimumPositionBtn = findViewById(R.id.btn1); // same to the button object
         Toolbar toolbar = findViewById(R.id.toolbar); // find the ui tool bar set in the activity main by default
         setSupportActionBar(toolbar); // android void to access the toolbar and set it as an object
-        getSupportActionBar().setTitle("Pocket Chiropractor"); // changing the title of the toolbar
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE); // set the sensor manager to a sensor service to access class values
+        Objects.requireNonNull(Objects.requireNonNull(getSupportActionBar())).setTitle("Pocket Chiropractor"); // changing the title of the toolbar
+        final SharedPreferences sharedPreferences = getPreferences(Context.MODE_PRIVATE);
+        MinimumPosition = sharedPreferences.getFloat(KEY,0f);
+        SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE); // set the sensor manager to a sensor service to access class values
+        assert sensorManager != null;
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER); // do the same to the acceleometer
         sensorManager.registerListener(MainActivity.this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL); //create a listener that is in this class under this context using the sensor accemeter
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putFloat(KEY, MinimumPosition);
+        cPosition.setText(" Minimum: "+ MinimumPosition); // set our min text
+        // Replace `putInt` with `putString` if your value is a String and not an Integer.
+        editor.apply();
+
+        if(MinimumPosition !=0){
+            cPosition.setText(" Minimum: "+ MinimumPosition); // set our min text
+            PositionSet =true;
+
+        }
+
         SelectMinimumPositionBtn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) { // if the button is pressed this will run
-                // start static flow void inside of on value changed in response to button click
-                PositionSet = true;
+            public void onClick(View v) {
+                PositionSet = true; // start static flow void inside of on value changed in response to button click
+                cPosition.setText(" Minimum: "+ MinimumPosition); // set our min text
             }
         });
+
+
     }
 
     // this void is abstract in Sensor Event Listener but does not preform a function
@@ -59,40 +80,50 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) { // if the accelometer changes value this void is triggered
-        Y_Value.setText("Position:  " + String.format("%.2f", sensorEvent.values[1])); //set text to current y position
+        cPosition.setText(" Minimum: "+ MinimumPosition); // set our min text
+        Y_Value.setText("Position:" + String.format("%.2f", sensorEvent.values[1])); //set text to current y position
         PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
-        if (sensorEvent.values[1] < MinimumPosition && pm.isScreenOn()  ) { //if current postion is less than minimum
+        assert pm != null;
+        if(MinimumPosition<0){
+            MinimumPosition=0;
+            cPosition.setText(" Minimum: "+ MinimumPosition); // set our min text
+            PositionSet=false;
+        }
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (sensorEvent.values[1] < MinimumPosition && pm.isInteractive()) { //if current postion is less than minimum
+            Log.println(Log.DEBUG,"bad","Too Low Correct Your Posture");
+            String message = "Too Low Correct Your Posture";
+            String NOTIFICATION_CHANNEL_ID = "my_channel_id_01";
 
-            String message = "You have been out of your PositionSet range for too long.";
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this) // create a notification
-                    .setSmallIcon(R.drawable.ic_spine) // find icon and set it to notification
-                    .setContentTitle("Correct Your Posture") // message to user
+            Uri uri = RingtoneManager
+                    .getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
 
-                    .setContentText(message) // set content as the string
-                    .setAutoCancel(true); //  can be turned on without user
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "My Notifications", NotificationManager.IMPORTANCE_DEFAULT);
+                notificationChannel.setDescription(message);        // Configure the notification channel.
+                notificationChannel.enableLights(true);
+                notificationChannel.setLightColor(Color.RED);
+                notificationChannel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
+                notificationChannel.enableVibration(true);
+                assert notificationManager != null;
+                notificationManager.createNotificationChannel(notificationChannel);
+            }
 
+            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
 
-            Intent intent = new Intent(MainActivity.this, NotificationActivity.class); //create an intent as notification
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            intent.putExtra("message", message);
-
-            PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this,
-                    0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-            builder.setContentIntent(pendingIntent);
-
-            NotificationManager notificationManager = (NotificationManager) getSystemService(
-                    Context.NOTIFICATION_SERVICE
-            );// build and push the notification
-
-            Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-            builder.setSound(uri); // create sound on notification
-            notificationManager.notify(0, builder.build());
-
-        } else {
-            NotificationManager notificationManager = (NotificationManager) getSystemService(
-                    Context.NOTIFICATION_SERVICE
-            );
-            notificationManager.cancel(0); // turn off the notification on fixed position
+            notificationBuilder.setAutoCancel(true)
+                    .setDefaults(Notification.DEFAULT_SOUND)
+                    .setWhen(System.currentTimeMillis())
+                    .setSmallIcon(R.drawable.ic_spine)
+                    .setTicker("Hearty365")
+                    .setPriority(Notification.DEFAULT_SOUND)
+                    .setContentTitle("Correction")
+                    .setContentText(message)
+                    .setContentInfo("Correction");
+            assert notificationManager != null;
+            notificationManager.notify(/*notification id*/1, notificationBuilder.build());
+        }else if(sensorEvent.values[1] > MinimumPosition ){
+            Log.println(Log.DEBUG,"bad","Should stop");
         }
         if (PositionSet) { //Control flow of void
             MinimumPosition = sensorEvent.values[1]; //set minimum position to current
